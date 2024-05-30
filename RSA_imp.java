d008bc26d277e6cf6e8bf9781b1fb40e8d38ee24
Ramisa Haque <rhaque40@stuy.edu>
2024-05-29 23:32:36 -0400

    encyrption and decryption algorithm imp'd; need makefile

--- RSA_imp.java
+++ RSA_imp.java
@@ -13,15 +13,18 @@ public class RSA_imp{
 		while(e <=1 && e.gcd(z) != 1){
 			e = probablePrime(2048, new Random());
 		}
+		d = e.modPow(-1, z);
 		// now msut find d such that (e * d)mod(z)=1
 		// public key: (n, e)
 		// private key: (n, d)
+		// cipher text is C = P^e mod n
+		// plain text is P = C^d mod n
 	}
 
-	public static BigInteger inverse(BigInteger e, BigInteger lambda_n){
-		BigInteger inv;
-		inv = e
-		return inv;
-		// BigInteger has a modPow(bigInt exponent, bigInt m) method that returns (this^exponent) mod m
-	}
+	// public static BigInteger inverse(BigInteger e, BigInteger lambda_n){
+	// 	return inv;
+	// 	// BigInteger has a modPow(bigInt exponent, bigInt m) method that returns (this^exponent) mod m
+	// }
+
+
 }
